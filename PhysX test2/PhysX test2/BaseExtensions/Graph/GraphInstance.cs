b0ad96988ddbda0952cc9e26d6dfdc864143e8be
KatekovAnton <void0main@gmail.com>
2011-10-28 03:10:06 +0400

    files

---
+++ PhysX test2/PhysX test2/BaseExtensions/Graph/GraphInstance.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PhysX_test2.BaseExtensions.Graph
+{
+    public class GraphInstance
+    {
+        public GraphStatic _baseGraph;
+
+        public GraphNode _currentNode;
+        public GraphNodeInctance _currentNodeInstance;
+
+        public GraphInstance(GraphStatic __baseGraph)
+        {
+            _baseGraph = __baseGraph;
+            _currentNode = _baseGraph._nodes[0];
+        }
+
+        protected GraphNode Advance(string __eventName)
+        {
+            GraphNode nextNode = _currentNode.GetNextNode(__eventName);
+            if (nextNode != null)
+                _currentNode = nextNode;
+            else
+            {
+                //event unused
+            }
+            return _currentNode;
+        }
+    }
+}
---
+++ PhysX test2/PhysX test2/BaseExtensions/Graph/GraphNodeInctance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PhysX_test2.BaseExtensions.Graph
+{
+    public class GraphNodeInctance
+    {
+        public GraphNode _baseNode;
+
+        public GraphNodeInctance(GraphNode _node)
+        {
+            _baseNode = _node;
+        }
+    }
+}
