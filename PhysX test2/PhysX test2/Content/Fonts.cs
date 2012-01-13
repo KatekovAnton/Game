5da73cbac6ec026f01cee1a4a15d50935287aff9
lojkofob <lojkofob@github.com>
2012-01-13 19:15:27 +0300

    Fonts, UserInterface, Controls"

---
+++ PhysX test2/PhysX test2/Content/Fonts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PhysX_test2.Content
+{
+    public static class Fonts
+    {
+        public static SpriteFont _font1;
+        public static void Load(string skin)
+        {
+            _font1 = Program.game.Content.Load<SpriteFont>("Courier New");
+        }
+    }
+}
---
+++ PhysX test2/PhysX test2/UserInterface/BaseInterface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PhysX_test2.UserInterface
+{
+    public class BaseInterface
+    {
+
+    }
+}
