f1c73cc34d65570b0cc3359a8170f760ba6dabef
KatekovAnton <void0main@gmail.com>
2012-01-18 14:39:17 +0400

    notifications

--- PhysX test2/PhysX test2/TheGame/Objects/GameParticleObject.cs
+++ PhysX test2/PhysX test2/TheGame/Objects/GameParticleObject.cs
@@ -3,9 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+
+using PhysX_test2.Engine;
+using PhysX_test2.Engine.Logic;
+using PhysX_test2.BaseExtensions.Graph;
+
+using PhysX_test2.TheGame;
+
+using PhysX_test2.TheGame.Level;
+using PhysX_test2.TheGame.Objects.StateGraphs;
+
+using Microsoft.Xna.Framework;
+
 namespace PhysX_test2.TheGame.Objects
 {
-    class GameParticleObject
+    public class GameParticleObject:GameObject
     {
+
+        public ParticleObject _object;
+
+        public GameParticleObject(GameLevel __level, Vector3 __maxSize, int __count, Vector3 __position, Vector3 __direction, float __gravityMult)
+            :base(__level,false,false)
+        {
+            _object = new ParticleObject(MyGame.UpdateTime.TotalGameTime, __maxSize, __count, __position, __direction, __gravityMult);
+        }
     }
 }
