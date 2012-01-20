4b1033ffd2559422672f247588456920978e1535
KatekovAnton <void0main@gmail.com>
2012-01-20 12:55:49 +0400

    fixed texture

--- PhysX test2/PhysX test2/Engine/Render/ParticleRenderObject.cs
+++ PhysX test2/PhysX test2/Engine/Render/ParticleRenderObject.cs
@@ -14,7 +14,7 @@ namespace PhysX_test2.Engine.Render
         /// Enum describes the various possible techniques
         /// that can be chosen to implement instancing.
         /// </summary>
-        static VertexDeclaration instanceVertexDeclaration = new VertexDeclaration
+        private static VertexDeclaration instanceVertexDeclaration = new VertexDeclaration
          (
              new VertexElement(0, VertexElementFormat.Vector4, VertexElementUsage.BlendWeight, 0),
              new VertexElement(16, VertexElementFormat.Vector4, VertexElementUsage.BlendWeight, 1),
@@ -25,7 +25,6 @@ namespace PhysX_test2.Engine.Render
         private Matrix[] _matrices;
         public MyModel _model;
         private DynamicVertexBuffer _instanceVertexBuffer;
-
       
         public ParticleRenderObject(MyModel __model, bool __ShadowCaster,
                                     bool __ShadowReceiver,
@@ -53,7 +52,6 @@ namespace PhysX_test2.Engine.Render
                     DrawModelNoInstancing();
                     break;
             }
-
         }
 
         /// <summary>
@@ -61,7 +59,6 @@ namespace PhysX_test2.Engine.Render
         /// </summary>
         void DrawModelHardwareInstancing()
         {
-
             if (_matrices.Length == 0)
                 return;
 
@@ -96,9 +93,7 @@ namespace PhysX_test2.Engine.Render
                                                        _model.subsets[0].mesh.VertexBuffer.VertexCount, 0,
                                                        _model.subsets[0].mesh.IndexBuffer.IndexCount / 3, _matrices.Length);
             }
-
         }
-
 
         /// <summary>
         /// Draws several copies of a piece of geometry without using any
@@ -107,6 +102,9 @@ namespace PhysX_test2.Engine.Render
         /// </summary>
         void DrawModelNoInstancing()
         {
+            if (_matrices.Length == 0)
+                return;
+
             MyGame.Device.SetVertexBuffer(_model.subsets[0].mesh.VertexBuffer);
             MyGame.Device.Indices = _model.subsets[0].mesh.IndexBuffer;
 
@@ -126,7 +124,6 @@ namespace PhysX_test2.Engine.Render
                     _model.subsets[0].mesh.Render();
                 }
             }
-
         }
 
         public override void Dispose()
@@ -140,68 +137,13 @@ namespace PhysX_test2.Engine.Render
                 base.Dispose();
             }
         }
-
-        /// <summary>
-        /// This technique is NOT a good idea! It is only included in the sample
-        /// for comparison purposes, so you can compare its performance with the
-        /// other more sensible approaches. This uses the exact same shader code
-        /// as the preceding NoInstancing technique, but with a key difference.
-        /// Where the NoInstancing technique worked like this:
-        /// 
-        ///     SetRenderStates()
-        ///     foreach instance
-        ///     {
-        ///         Update effect with per-instance transform matrix
-        ///         DrawIndexedPrimitives()
-        ///     }
-        /// 
-        /// NoInstancingOrStateBatching works like so:
-        /// 
-        ///     foreach instance
-        ///     {
-        ///         Set per-instance transform matrix into the effect
-        ///         SetRenderStates()
-        ///         DrawIndexedPrimitives()
-        ///     }
-        ///      
-        /// As you can see, this is repeatedly setting the same renderstates.
-        /// Not so efficient.
-        /// 
-        /// In other words, the built-in Model.Draw method is pretty inefficient when
-        /// it comes to drawing more than one instance! Even without using any fancy
-        /// shader techniques, you can get a significant speed boost just by rearranging
-        /// your drawing code to work more like the earlier NoInstancing technique.
-        /// </summary>
-        /*   void DrawModelNoInstancingOrStateBatching(MyModel model, Matrix[] modelBones,
-                                                     Matrix[] instances, Matrix view, Matrix projection)
-           {
-               for (int i = 0; i < instances.Length; i++)
-               {
-                   foreach (ModelMesh mesh in model.Meshes)
-                   {
-                       foreach (Effect effect in mesh.Effects)
-                       {
-                           effect.CurrentTechnique = effect.Techniques["NoInstancing"];
-
-                           effect.Parameters["World"].SetValue(modelBones[mesh.ParentBone.Index] * instances[i]);
-                           effect.Parameters["View"].SetValue(view);
-                           effect.Parameters["Projection"].SetValue(projection);
-                       }
-
-                       mesh.Draw();
-                   }
-               }
-           }*/
-
-
+       
         /// <summary>
         /// Set data in update for using it in draw()
         /// </summary>
         /// <param name="matrices">Matrices per particle</param>
         public void SetParticleData(Matrix[] matrices)
         {
-
-
             _matrices = matrices;
         }
     }
