896b0c0804c806879311d28155d3e224c6d4a5e2
My workplace <Admin@.(none)>
2012-02-10 11:26:50 +0300

    new files

---
+++ PhysX test2/PhysX test2/BaseExtensions/Curve.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+
+namespace PhysX_test2
+{
+    public class Curve
+    {
+        public class CurveStep
+        {
+            public float _start;
+            public float _end;
+            public CurveStep(float __start, float __end)
+            {
+                _start = __start;
+                _end = __end;
+            }
+        }
+
+        public float _CureveStepLength;
+        public CurveStep[] _steps;
+
+        public Curve( float __start, float __end)
+        {
+            _steps = new CurveStep[1];
+            _steps[0] = new CurveStep(__start, __end);
+        }
+
+        public void AddStep(float __delta)
+        {
+            List<CurveStep> steps = new List<CurveStep>(_steps);
+            steps.Add(new CurveStep(steps[steps.Count - 1]._end, steps[steps.Count - 1]._end + __delta));
+            _steps = steps.ToArray();
+        }
+
+        public float GetValue(double _x)
+        {
+            int fullpart = (int)_x;
+            float ostatok = (float)(_x - Math.Floor(_x));
+
+            fullpart = fullpart % _steps.Length;
+
+            CurveStep cs = _steps[fullpart];
+
+            return MathHelper.Lerp(cs._start, cs._end, ostatok);
+        }
+    }
+}
---
+++ PhysX test2/PhysX test2/Engine/Logic/PivotObjectRenderMatrixController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PhysX_test2.Engine.Logic
+{
+    interface PivotObjectRenderMatrixController
+    {
+        Matrix GetRenderMatrix(Matrix __baseTransformMatrix);
+    }
+
+    class BillboardMatrixController : PivotObjectRenderMatrixController
+    {
+        public Matrix GetRenderMatrix(Matrix __baseTransformMatrix)
+        {
+            return Matrix.CreateBillboard(__baseTransformMatrix.Translation, CameraControllers.CameraManager.Camera._position, Vector3.Up, CameraControllers.CameraManager.Camera._direction);
+        }
+    }
+
+    class ConstrainedBillboardMatrixController : PivotObjectRenderMatrixController
+    {
+        private PivotObject _baseObject;
+        public ConstrainedBillboardMatrixController(PivotObject __baseObject)
+        {
+            _baseObject = __baseObject;
+        }
+
+        public Matrix GetRenderMatrix(Matrix __baseTransformMatrix)
+        {
+            return Matrix.CreateConstrainedBillboard(__baseTransformMatrix.Translation, CameraControllers.CameraManager.Camera._position, _baseObject._objectConstrAxis, CameraControllers.CameraManager.Camera._direction, _baseObject._objectConstrForward);
+        }
+    }
+
+    class SkewMatrixController : PivotObjectRenderMatrixController
+    {
+        private double _x, _y;
+
+        private static Vector2 _winddirection = new Vector2(-0.1f, 0.1f);
+        private static float _windPower = 1.5f;
+        private float _scale = 0.1f;
+        private static Vector2 _maxDispersion = new Vector2(0.1f, 0.1f);
+
+        public SkewMatrixController(float __scale)
+        {
+            _scale = __scale;
+        }
+
+        public Matrix GetRenderMatrix(Matrix __baseTransformMatrix)
+        {
+            double deltax = MyRandom.NextDouble(_maxDispersion.X);
+            double deltay = MyRandom.NextDouble(_maxDispersion.Y);
+
+            double time = MyGame.UpdateTime.ElapsedGameTime.TotalSeconds * 600 * _scale;
+
+            _x += deltax * time;
+            _y += deltay * time;
+            return Extensions.CreateSkew((_winddirection.X * _windPower + (float)Math.Sin(_x)) * _scale, (_winddirection.Y * _windPower + (float)Math.Sin(_y)) * _scale) * __baseTransformMatrix;
+        }
+    }
+}
